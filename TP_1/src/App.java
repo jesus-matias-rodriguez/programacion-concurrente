687551c2e0e0248ecd47e4924a3c76e830225f2e
jesus matias rodriguez <jesus.rodriguez@fi.unju.edu.ar>
2024-08-24 22:18:08 -0300

    refactor(app): se adapto a los cambios del paquete forma

--- TP_1/src/App.java
+++ TP_1/src/App.java
@@ -6,23 +6,23 @@ import formas.IForma;
 class App {
     private static void print_forma(IForma forma) {
         System.out.println(
-                "Forma: " + forma.NombreForma() + "\n" +
-                        "Perímetro: " + forma.perimetro() + "\n" +
-                        "Area: " + forma.area() + "\n");
+            "Forma: "     + forma.getNombreForma() + "\n" +
+            "Perímetro: " + forma.perímetro()      + "\n" +
+            "Area: "      + forma.area()           + "\n"
+        );
     }
 
     private static void print_forma(Forma forma) {
         System.out.println(
-                "Forma: " + forma.NombreForma + "\n" +
-                        "Perímetro: " + forma.perimetro() + "\n" +
-                        "Area: " + forma.area() + "\n");
+            "Forma: "     + forma.getNombreForma() + "\n" +
+            "Perímetro: " + forma.perimetro()      + "\n" +
+            "Area: "      + forma.area()           + "\n"
+        );
     }
 
     public static void main(String[] args) {
         var rect = new Cuadrilatero(3.0, 2.0);
         print_forma(rect);
-
-        rect.area();
 
         var circ = new Circulo(1.0);
         print_forma(circ);
